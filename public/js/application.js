2a133dc6b1124c95015957fee6731a776f63dd39
mjeannin <marine.jeannin@sciencespo.fr>
2017-10-09 19:31:15 +0200

    fucking scroll

--- public/js/application.js
+++ public/js/application.js
@@ -8,8 +8,16 @@ $(".button-collapse").sideNav();
 // ];
 // Materialize.scrollFire(options);
 
+// var options = [
+//     {selector: '#staggered-test', offset: 50, callback: function(el) { Materialize.toast("This is our ScrollFire Demo!", 1500 ); } },
+//     {selector: '#staggered-test', offset: 205, callback: function(el) { Materialize.toast("Please continue scrolling!", 1500 ); } },
+//     {selector: '#staggered-test', offset: 400, callback: function(el) { Materialize.showStaggeredList($(el)); } },
+//     {selector: '#image-test', offset: 500, callback: function(el) { Materialize.fadeInImage($(el)); } } ];
+// Materialize.scrollFire(options);
+
+
 var options = [
-    {selector: '#line1', offset: 1000, callback: function(el) { Materialize.fadeInImage($(el)); } },
-    {selector: '#line2', offset: 1500, callback: function(el) { Materialize.fadeInImage($(el)); } }
+    {selector: '#line3', offset: 400, callback: function(el) { Materialize.fadeInImage($(el)); } },
+    {selector: '#line4', offset: 500, callback: function(el) { Materialize.fadeInImage($(el)); } }
 ];
 Materialize.scrollFire(options);
