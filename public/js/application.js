017579419205c5f8c1999ed0845154355fc5acab
mjeannin <marine.jeannin@sciencespo.fr>
2017-10-09 19:23:01 +0200

    articles ajoutes et qq bugs fixed

--- public/js/application.js
+++ public/js/application.js
@@ -1,1 +1,15 @@
 $(".button-collapse").sideNav();
+
+// var options = [
+//     {selector: '.class', offset: 200, callback: customCallbackFunc },
+//     {selector: '.other-class', offset: 200, callback: function() {
+//         customCallbackFunc();
+//     } }
+// ];
+// Materialize.scrollFire(options);
+
+var options = [
+    {selector: '#line1', offset: 1000, callback: function(el) { Materialize.fadeInImage($(el)); } },
+    {selector: '#line2', offset: 1500, callback: function(el) { Materialize.fadeInImage($(el)); } }
+];
+Materialize.scrollFire(options);
