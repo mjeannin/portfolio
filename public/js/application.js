41956d7659661664689e5eae21fe4e69f0056efa
mjeannin <marine.jeannin@sciencespo.fr>
2017-10-09 21:42:33 +0200

    debut de scrolling

--- public/js/application.js
+++ public/js/application.js
@@ -17,7 +17,30 @@ $(".button-collapse").sideNav();
 
 
 var options = [
-    {selector: '#line3', offset: 400, callback: function(el) { Materialize.fadeInImage($(el)); } },
-    {selector: '#line4', offset: 500, callback: function(el) { Materialize.fadeInImage($(el)); } }
+    {selector: '#line3', offset: 1250, callback: function(el) { Materialize.fadeInImage($(el)); } },
+    {selector: '#line4', offset: 2100, callback: function(el) { Materialize.fadeInImage($(el)); } },
+    {selector: '#line5', offset: 2600, callback: function(el) { Materialize.fadeInImage($(el)); } },
+    {selector: '#line6', offset: 3100, callback: function(el) { Materialize.fadeInImage($(el)); } },
+    {selector: '#line7', offset: 3500, callback: function(el) { Materialize.fadeInImage($(el)); } },
+    {selector: '#line8', offset: 4000, callback: function(el) { Materialize.fadeInImage($(el)); } },
+    {selector: '#line9', offset: 4800, callback: function(el) { Materialize.fadeInImage($(el)); } },
+    {selector: '#line10', offset: 5800, callback: function(el) { Materialize.fadeInImage($(el)); } },
+    {selector: '#line11', offset: 0, callback: function(el) { Materialize.fadeInImage($(el)); } },
+    {selector: '#line12', offset: 0, callback: function(el) { Materialize.fadeInImage($(el)); } },
+    {selector: '#line13', offset: 0, callback: function(el) { Materialize.fadeInImage($(el)); } },
+    {selector: '#line14', offset: 0, callback: function(el) { Materialize.fadeInImage($(el)); } },
+    {selector: '#line15', offset: 0, callback: function(el) { Materialize.fadeInImage($(el)); } },
+    {selector: '#line16', offset: 0, callback: function(el) { Materialize.fadeInImage($(el)); } },
+    {selector: '#line17', offset: 0, callback: function(el) { Materialize.fadeInImage($(el)); } },
+    {selector: '#line18', offset: 0, callback: function(el) { Materialize.fadeInImage($(el)); } },
+    {selector: '#line19', offset: 0, callback: function(el) { Materialize.fadeInImage($(el)); } },
+    {selector: '#line20', offset: 0, callback: function(el) { Materialize.fadeInImage($(el)); } },
+    {selector: '#line21', offset: 0, callback: function(el) { Materialize.fadeInImage($(el)); } },
+    {selector: '#line22', offset: 0, callback: function(el) { Materialize.fadeInImage($(el)); } },
+    {selector: '#line23', offset: 0, callback: function(el) { Materialize.fadeInImage($(el)); } },
+    {selector: '#line24', offset: 0, callback: function(el) { Materialize.fadeInImage($(el)); } },
+    {selector: '#line25', offset: 0, callback: function(el) { Materialize.fadeInImage($(el)); } },
+    {selector: '#line26', offset: 0, callback: function(el) { Materialize.fadeInImage($(el)); } },
+    {selector: '#line27', offset: 0, callback: function(el) { Materialize.fadeInImage($(el)); } },
 ];
 Materialize.scrollFire(options);
