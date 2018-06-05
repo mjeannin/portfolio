06e33c1251aae39019a7f2e4ed123139e92b25e0
Marine JEANNIN <mjeannin@e2r2p4.42.fr>
2018-06-05 15:26:21 +0200

    json to html the beginning

---
+++ public/js/jsontohtml.js
@@ -0,0 +1,11 @@
+var jcontent = {
+    "Date": "11/08/2016",
+    "Titre": "La Manif pour tous entend peser sur la primaire de la droite",
+    "Media": "Paris Match (site)",
+    "Rubrique": "Politique",
+    "Chapo": "Le mouvement La Manif pour tous a appelé mercredi à une manifestation le 16 octobre pour défendre la famille «à nouveau menacée». En jeu: combattre la gestation pour autrui (GPA) et la procréation médicalement assistée (PMA). Un mois avant les primaires Républicaines, les candidats de la droite vont devoir prendre position.",
+    "Lien-de-l-article": "http://www.parismatch.com/Actu/Politique/La-Manif-pour-tous-entend-peser-sur-la-primaire-de-la-droite-1036768",
+    "Lien-de-l-image": "http://resize1-parismatch.ladmedia.fr/r/625,417,center-middle,ffffff/img/var/news/storage/images/paris-match/actu/politique/la-manif-pour-tous-entend-peser-sur-la-primaire-de-la-droite-1036768/14712187-1-fre-FR/La-Manif-pour-tous-entend-peser-sur-la-primaire-de-la-droite.jpg"
+};
+var jtitle = document.getElementById('jtitle');
+jtitle.innerHTML = jcontent.Titre;
