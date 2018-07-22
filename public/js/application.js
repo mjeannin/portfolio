c1ff780a2b0158ba7d8b004a15ce540b05913b30
mjeannin <mjeannin@student.42.fr>
2018-07-22 19:50:32 +0200

    it begins to work

--- public/js/application.js
+++ public/js/application.js
@@ -176,7 +176,7 @@ $(function() {
 
 /* Set the width of the side navigation to 250px */
 function openNav() {
-    document.getElementById("mySidenav").style.width = "250px";
+    document.getElementById("mySidenav").style.width = "300px";
 }
 
 /* Set the width of the side navigation to 0 */
