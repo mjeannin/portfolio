2606bb65adfb6501a69be02238535e22165d679b
mjeannin <mjeannin@student.42.fr>
2018-08-19 16:47:03 +0200

    jai rajoute ma gueule hihi

--- public/js/loadArticles.js
+++ public/js/loadArticles.js
@@ -20,7 +20,7 @@ function myFun(){
     let req = new XMLHttpRequest();
     req.onreadystatechange = function() {
         if (this.readyState == 4 && this.status == 200) {
-            renderEjsOnElement('<a target="_blank" href=<%=Lien %>> <div class="card-image large"> <img class="img-fit" src=<%=Image %>> <span class="card-title" id="jrub"><%=Titre %></span> </div><div class="card-content"> <span class="card-title" id="jtitle"><%=Chapo %></span> <p class="chapo" id="jchapo"></p></div><div class="card-action"> <a target="_blank" id="toto" class="media blue-text" href=<%=Lien %>>LINK</a> </div></a>'
+            renderEjsOnElement('<a target="_blank" href=<%=Lien %>> <div class="card-image large"> <img class="img-fit" src=<%=Image %>> <span class="card-title" id="jrub"><%=Titre %></span> </div><div class="card-content"> <span class="card-title" id="jtitle"><%=Chapo %></span> <p class="chapo" id="jchapo"></p></div><div class="card-action"> <a target="_blank" id="toto" class="media blue-text" href=<%=Lien %>><%=Media %></a> </div></a>'
                 , JSON.parse(this.responseText)
                 , 'article' + thisId);
         }
